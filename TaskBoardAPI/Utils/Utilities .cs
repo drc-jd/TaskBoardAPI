a505b5197754d3a0b78e270ee0bfe677f50bfd4b
karnavst <karnavst@ddpl.com>
2023-05-01 11:28:36 +0530

    utils..!

--- TaskBoardAPI/Utils/Utilities .cs
+++ TaskBoardAPI/Utils/Utilities .cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.IO;
 using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
@@ -57,6 +56,38 @@ namespace TaskBoardAPI.Utils
             }
             //put a breakpoint here and check datatable
             return dataTable;
+        }
+        public static string GetMd5HashWithMySecurityAlgo(string input)
+        {
+            MD5 md5Hash = MD5.Create();
+            // Convert the input string to a byte array and compute the hash.  
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            // Create a new Stringbuilder to collect the bytes  
+            // and create a string.  
+            StringBuilder sBuilder = new StringBuilder();
+            // Loop through each byte of the hashed data  
+            // and format each one as a hexadecimal string.  
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            // Return the hexadecimal string.  
+            return sBuilder.ToString();
+        }
+        public static bool VerifyMd5HashWithMySecurityAlgo(string input, string hash)
+        {
+            // Hash the input.  
+            string hashOfInput = GetMd5HashWithMySecurityAlgo(input);
+            // Create a StringComparer an compare the hashes.  
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
     public class ColumnCaption
